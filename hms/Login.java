7c9e82a959bf8d1a6a9fc3d31df0c8f73b2a6baf
HariKrishnan-Ramesh <harikrishnaramesh777@gmail.com>
2022-03-01 00:08:39 +0530

    code changes

--- hms/Login.java
+++ hms/Login.java
@@ -1,12 +1,23 @@
 package hms;
+import javax.swing.*;
 
-public class Login
-{
-    public Login()
-    {
-        System.out.println("HELLO ALL");
-        System.out.println("HELLO HARIIII");
-        New n = new New();
-        n.print_name("HARI KRISHNA");
+public class Login extends JFrame{
+
+    Login(){
+         setBounds(300,300,1366,565);
+     //    setSize(400,400);
+     //    setLocation(300,300);
+
+     ImageIcon i1 = new ImageIcon(ClassLoader.getSystemResource("hms/first.jpg"));
+    JLabel l1 = new Jlabel(il);
+     setBounds(0,0,1366,565);
+     setLayout(null);
+     setVisible(true);
+
+    } 
+
+    public static void main(String[] args){
+
+        new Login();
     }
 }
--- hms/New.java
+++
@@ -1,9 +0,0 @@
-package hms;
-
-public class New 
-{
-    public void print_name(String s)
-    {
-        System.out.println(s);
-    }
-}
