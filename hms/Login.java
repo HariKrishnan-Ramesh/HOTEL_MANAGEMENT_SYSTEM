cc5afd81c80dfa170e86b40b65f04f2af1629882
HariKrishnan-Ramesh <harikrishnaramesh777@gmail.com>
2022-02-28 20:53:48 +0530

    Login update

--- hms/Login.java
+++ hms/Login.java
@@ -5,7 +5,7 @@ public class Login
     public Login()
     {
         System.out.println("HELLO ALL");
-
+        System.out.println("HELLO HARIIII");
         New n = new New();
         n.print_name("HARI KRISHNA");
     }
